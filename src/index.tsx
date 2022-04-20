3d2946ec7b6ed3eda7ab01ab91c9c5b7d9ea72fc
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-04-19 21:58:15 -0300

    feat: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 import "./index.scss"
 import App from "./App"
+import Profile from "./routes/Profile"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/routes/Profile.tsx
@@ -0,0 +1,13 @@
+import "../App.scss"
+import { Link } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Perfil</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default App
