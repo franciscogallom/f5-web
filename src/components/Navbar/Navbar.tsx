1c7fb2f50732d0293353f26cef101318550e865b
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-05-02 15:23:47 -0300

    improvement: show active link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
 import { FC, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 import "./navbar.scss"
 import icon from "../../assets/icon-transparent.png"
 
 const Navbar: FC = () => {
   const [active, setActive] = useState(false)
+  const { pathname } = useLocation()
 
   return (
     <>
       <div className="navbar">
         <img className="icon" src={icon} alt="f5" />
         <div className={`links-container ${active ? "active" : ""}`}>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/profile"}>Mi cuenta</Link>
+          <Link className={pathname === "/" ? "active-link" : ""} to={"/"}>
+            Home
+          </Link>
+          <Link className={pathname === "/profile" ? "active-link" : ""} to={"/profile"}>
+            Mi cuenta
+          </Link>
         </div>
         {/* Burger Button */}
         <div
