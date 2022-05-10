651291ea6ba1aa0885f03f05d54f2b56937df216
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-05-10 11:10:19 -0300

    improvement: context with mongoID

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -5,6 +5,8 @@ type ContextType = {
   setUser: React.Dispatch<React.SetStateAction<string>>
   name: string
   setName: React.Dispatch<React.SetStateAction<string>>
+  mongoID: string
+  setMongoID: React.Dispatch<React.SetStateAction<string>>
 }
 
 const Context = createContext<ContextType>({} as ContextType)
@@ -12,8 +14,13 @@ const Context = createContext<ContextType>({} as ContextType)
 export const ContextProvider = ({ children }: any) => {
   const [user, setUser] = useState("")
   const [name, setName] = useState("")
+  const [mongoID, setMongoID] = useState("")
 
-  return <Context.Provider value={{ user, setUser, name, setName }}>{children}</Context.Provider>
+  return (
+    <Context.Provider value={{ user, setUser, name, setName, mongoID, setMongoID }}>
+      {children}
+    </Context.Provider>
+  )
 }
 
 export default Context
