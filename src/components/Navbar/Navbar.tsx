daad140416120d77487866a1d522973b495da514
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-05-09 22:01:40 -0300

    improvement: better Navbar UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
-import { FC, useState } from "react"
+import { FC, useContext, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 
 import "./navbar.scss"
 import icon from "../../assets/icon-transparent.png"
+import Context from "../../context/context"
 
 const Navbar: FC = () => {
   const [active, setActive] = useState(false)
+  const { name } = useContext(Context)
   const { pathname } = useLocation()
 
   return (
     <>
       <div className="navbar">
+        <h1 className="field-name">{name}</h1>
         <img className="icon" src={icon} alt="f5" />
         <div className={`links-container ${active ? "active" : ""}`}>
           <Link className={pathname === "/" ? "active-link" : ""} to={"/"}>
