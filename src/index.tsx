6441966b1301d20b24363880da5e44d41a0d1373
franciscogallom <franciscogallomolinuevo@gmail.com>
2022-05-14 21:12:45 -0300

    improvement: set auth token for all axios request

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import axios from "axios"
 
 import "./index.scss"
 import { Toaster } from "react-hot-toast"
@@ -9,6 +10,9 @@ import Profile from "./routes/Profile"
 import NotFound from "./routes/404"
 import Main from "./routes/Main"
 import SignUp from "./routes/SignUp"
+
+// Set authorization token for all axios request.
+axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")}`
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
